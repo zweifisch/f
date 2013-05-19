5c12015ba4eba8b2d70ec2992d5960b1219ce732
Feng Zhou <zf.pascal@gmail.com>
2013-05-19 13:56:50 +0800

    make singleton a trait

---
+++ f/Singleton.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace f;
+
+trait Singleton
+{
+	protected static $instance;
+	final public static function getInstance()
+	{
+		return isset(static::$instance)
+			? static::$instance
+			: static::$instance = new static;
+	}
+}
